24d3e558b19113910090cb11eb7fec28695cda3e
lueFlake <uselessal123@gmail.com>
2022-07-11 19:34:59 +0300

    csdcdc

---
+++ APICommentBook/Models/IStudyPart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICommentBook.Models
+{
+    interface IStudyPart
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ExternalId { get; set; }
+    }
+}
---
+++ APICommentBook/SimpleLogger.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace APICommentBook {
+    public class SimpleLogger {
+        public static SimpleLogger Instance { get; set; }
+        private StreamWriter _writer;
+
+        public SimpleLogger(string path) {
+            _writer = new StreamWriter(path);
+        }
+
+        ~SimpleLogger() {
+            _writer.Dispose();
+        }
+    }
+}
