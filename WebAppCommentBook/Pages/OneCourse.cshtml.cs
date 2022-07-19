f5288d595b642da29f8c53b4d10402cb2e5115d5
zemlianin <detulie@mail.ru>
2022-07-19 10:15:16 +0300

    Полная маршрутизация между страницами

--- WebAppCommentBook/Pages/OneCourse.cshtml.cs
+++ WebAppCommentBook/Pages/OneCourse.cshtml.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebAppCommentBook.Pages
 {
-    public class CoursesModel : PageModel
+    public class OneCourseModel : PageModel
     {
         public int externalId { get; set; }
 
@@ -18,11 +18,11 @@ namespace WebAppCommentBook.Pages
         {
             this.name = name;
             this.externalId = externalId;
-            var request = new RequestSender();
+            /*var request = new RequestSender();
 
             ViewData["Courses"] = JsonSerializer.Deserialize<List<BaseClass>>(
                 request.Get($"http://APICommentBook/Get-name-Courses-by-externalId?externalId=" + this.externalId).Result);
-            //System.IO.File.WriteAllText("output.txt", request.Get($"http://APICommentBook/Get-name-Courses-by-externalId?externalId=" + this.externalId).Result);
+            //System.IO.File.WriteAllText("output.txt", request.Get($"http://APICommentBook/Get-name-Courses-by-externalId?externalId=" + this.externalId).Result);*/
         }
     }
 }
