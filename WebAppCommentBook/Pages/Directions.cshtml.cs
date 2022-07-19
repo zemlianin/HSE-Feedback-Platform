0b55a00a9d32f4b74987eaa088d46f7eb97736e5
zemlianin <detulie@mail.ru>
2022-07-19 10:15:16 +0300

    Полная маршрутизация между страницами

--- WebAppCommentBook/Pages/Directions.cshtml.cs
+++ WebAppCommentBook/Pages/Directions.cshtml.cs
@@ -13,14 +13,17 @@ namespace WebAppCommentBook.Pages
     public class DirectionsModel : PageModel
     {
         public int externalId { get; set; }
-        public void OnGet(int externalId)
+
+        public string name { get; set; }
+        public void OnGet(int externalId,string name)
         {
+            this.name = name;
             this.externalId= externalId;
             var request = new RequestSender();
             
             ViewData["Directions"] = JsonSerializer.Deserialize<List<BaseClass>>(
                 request.Get($"http://APICommentBook/Get-name-Directions-by-externalId?externalId="+externalId).Result);
-           System.IO.File.WriteAllText("output.txt", externalId.ToString());
+         //  System.IO.File.WriteAllText("output.txt", externalId.ToString());
         }
     }
 }
